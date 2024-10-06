73ab6c34b6ceb3ee3438a6eeee58ddab1cf17155
Thanh Tuyen <tuyen13022003@gmail.com>
2024-10-06 21:14:07 +0700

    hello

---
+++ config/database.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+mongoose.set("strictQuery", false);
+
+const connectDB = async () => {
+  try {
+    const uri = process.env.MONGODB_URI;
+    if (!uri) {
+      throw new Error(
+        "MONGODB_URI is not defined in the environment variables"
+      );
+    }
+    console.log("Attempting to connect with URI:", uri);
+    const conn = await mongoose.connect(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error(`Error: ${error.message}`);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
